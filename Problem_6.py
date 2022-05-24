d573ac1443a46478f5284f741984d437199907fc
Juan Moreira <juan.moreira@dellead.com>
2022-05-24 01:29:27 -0300

    Update Problem_6.py

--- Problem_6.py
+++ Problem_6.py
@@ -1,1 +1,12 @@
-
+# url = https://projecteuler.net/problem=6
+def s_square_n(n):
+  s = (n*(n+1))/2
+  s = s*s
+  return s
+def s_n_square(n):
+  s = n*((2*n)+1)*(n+1)/6 
+  return s
+def square_dif(n):
+  s =  s_square_n(n) - s_n_square(n)
+  return s
+print(square_dif(100))
